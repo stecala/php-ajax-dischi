52ebe9a7da1158afef4971f6980901556414bd07
stecala <ste.cala123@gmail.com>
2022-08-25 15:07:42 +0200

    ⭐ Feat : Milestone 1 complete

--- index_php.php
+++ index_php.php
@@ -17,7 +17,37 @@
        include __DIR__ . '/db/db.php';
     ?>
     <header>
-        
+    
     </header>
+    <main>
+        <div class="container-lg ">
+            <div class="row my-margin">
+                <?php foreach( $albums as $album){ ?> 
+                    <div class="col-2 card pt-3 m-2">
+                        <div class="row">
+                            <div class="col-12 img-container">
+                                <img src="<?php echo $album['poster'] ?>" alt=" <?php echo $album['title'] ?> poster " >
+                            </div>
+                            <div class="col-12 pt-3 text-center text-white">
+                                <h4>
+                                    <?php echo $album['title'] ?>
+                                </h4>
+                            </div>
+                            <div class="col-12 text-center pt-3 ">
+                                <h6>
+                                    <?php echo $album['author'] ?>
+                                </h6>
+                            </div>
+                            <div class="col-12 pt-1 text-center">
+                                <h6>
+                                    <?php echo $album['year'] ?>    
+                                </h6>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
