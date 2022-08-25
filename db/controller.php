5c343a4f52d9231e57b439bebf1ef7a80e548013
stecala <ste.cala123@gmail.com>
2022-08-25 16:55:36 +0200

    ⭐ Feat : Bonus complete

--- db/controller.php
+++ db/controller.php
@@ -3,4 +3,23 @@
 
     header('Content-Type: application/json');
 
-    echo json_encode($albums);
+
+
+    if(isset($_GET['genre'])){
+        $genre = $_GET['genre'];
+        $filteredAlbums = [];
+        foreach($albums as $album){
+            if($album['genre'] == $genre){
+                $filteredAlbums[] = $album;
+            }
+        }
+        echo json_encode($filteredAlbums);
+    }
+    else{
+        echo json_encode($albums);
+    }
+    
+    
+    
+    
+    
--- script/script.js
+++ script/script.js
@@ -2,22 +2,28 @@ const app = new Vue({
     el: '#app',
     data: {
         discsList : [],
-    
+        genreToSearch : null,
+        genres : ['Rock', 'Pop', 'Jazz', 'Metal'],
+        selected : '',
     },
     methods: {
-        APICallDiscs(){
-            axios.get('http://localhost/php-ajax-dischi/db/controller.php')
+        APICallDiscs(genre){
+            let url = 'http://localhost/php-ajax-dischi/db/controller.php'
+            if(genre){
+                url += '?genre='+genre
+            }
+            axios.get(url)
             .then((result)=>{
-            this.discsList = result.data
-            console.log(this.discsList)
+                this.discsList = result.data
             })
             .catch((error)=>{
             console.error(error)
             })
-        }
+        },
+
     },
     created(){
-        this.APICallDiscs()
+        this.APICallDiscs(this.genreToSearch)
     }
 
 })
